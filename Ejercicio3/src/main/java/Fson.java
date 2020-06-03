350de55459a40a52cb835216641ad97c93a5dd42
Facundo Decena <facundo.decena@gmail.com>
2020-06-03 19:38:05 -0300

    Ejercicio3

--- Ejercicio3/src/main/java/Fson.java
+++ Ejercicio3/src/main/java/Fson.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import java.lang.reflect.Field;
 
 public class Fson {
+    // Returns the json format of o
+    public static String toFson(Object o){
+        StringBuilder fson = new StringBuilder("{");
+        // Tomo la clase
+        Class<?> c = o.getClass();
+        // Y sus campos
+        Field[] fields = c.getFields();
+        // Para cada campo
+        for (Field field : fields) {
+            String fieldName = field.getName();
+            // Agrego el nombre a la respuesta
+            fson.append("\"").append(fieldName).append("\":");
+            Class<?> typeClass = field.getType();
+            // Si es un objeto compuesto hago lo mismo sobre ese objeto
+            if (typeClass.getFields().length > 1) {
+                try {
+                    fson.append(toFson(field.get(o))).append(",");
+                } catch (IllegalAccessException ignored) {
+                    fson.append("\"").append("\",");
+                }
+            }else {
+                try {
+                    Object fieldValue = field.get(o);
+                    fson.append("\"").append(fieldValue).append("\",");
+                } catch (IllegalAccessException ignored) {
+                    fson.append("\"").append("\",");
+                }
+            }
+        }
+        // Elimino la ultima coma y agrego la llave que cierra
+        fson.deleteCharAt(fson.lastIndexOf(","));
+        fson.append("}");
+        return fson.toString();
+    }
+
 }
--- Ejercicio3/src/main/java/Main.java
+++ Ejercicio3/src/main/java/Main.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(Fson.toFson(new objetoRandom()));
+    }
+}
+
+class objetoRandom {
+    public String field1;
+    public small field2;
+    public big field3;
+
+    objetoRandom() {
+        field1 = "asd";
+        field2 = new small();
+        field3 = new big();
+    }
+}
+
+class small {
+    public int field2_1;
+    public float field2_2;
+
+    small() {
+        field2_1 = 3;
+        field2_2 = 4.2f;
+    }
+}
+
+class big {
+    public small field3_1;
+    public float field3_2;
+    public String field3_3;
+
+    big() {
+        field3_1 = new small();
+        field3_2 = 54.3216f;
+        field3_3 = "aksndlaksd";
+    }
 }
